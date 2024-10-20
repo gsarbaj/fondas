c27cc4f2db2c00e60eb20418e3ffdb6cfed874a2
g.sarbaj@icloud.com <g.sarbaj@icloud.com>
2024-10-20 20:06:13 +0300

    page fixed

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,19 +1,9 @@
 import {useTranslations} from "next-intl";
-import { getRequestConfig } from "next-intl/server";
-import { cookies, headers } from "next/headers";
 
 export default function Home() {
 
     const t = useTranslations("HomePage")
 
-    const userLanguage = navigator.language
-    console.log(userLanguage); // e.g., "en-US"
-
-    const headersList = headers();
-    const defaultLocale = headersList.get("accept-language");
-    const locale = cookies().get("NEXT_LOCALE")?.value
-
-    console.log(locale)
 
     return (
         <>
